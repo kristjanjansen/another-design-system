2e3f0dd20949e0ae643f57211c16d5f386dadf5d
Kristjan Jansen <kristjan.jansen@gmail.com>
2024-05-04 22:34:54 +0300

    fix types

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import preserveDirectives from "rollup-preserve-directives";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), preserveDirectives()],
+  plugins: [react(), preserveDirectives(), dts({ rollupTypes: true })],
   build: {
     outDir: "./dist",
     lib: {
