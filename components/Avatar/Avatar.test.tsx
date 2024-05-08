98e7945c1d77527819daf4df9cc2b0b4f886bb1e
Kristjan Jansen <kristjan.jansen@gmail.com>
2024-05-08 12:30:55 +0300

    cleanup

--- components/Avatar/Avatar.test.tsx
+++ components/Avatar/Avatar.test.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { describe, test, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, test } from "vitest";
+import { render } from "@testing-library/react";
 
 import Avatar from "./Avatar";
 
 describe("Avatar", () => {
   test("renders without errors", async () => {
-    render(<Avatar />);
+    render(<Avatar imageUrl="" />);
   });
 });
